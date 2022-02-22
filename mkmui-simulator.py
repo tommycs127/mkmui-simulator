c00e763101c9e0a9ca2f5f3185157a74aa71beaf
Wadadon <35282459+tommycs127@users.noreply.github.com>
2022-02-23 01:18:17 +0800

    大改
    - 個 client 改用 class 嚟寫
    - 將啲句子同稱呼拆晒做 class 嚟寫，啲 option 用嚟決定句嘢可以點 generate 出嚟
    - 扠晒啲太尷或者模稜兩可嘅回覆，同時又加咗啲新嘅
    - 徹底放棄 sticker
    - 斷線自動恢復（測試緊）

--- mkmui-simulator.py
+++ mkmui-simulator.py
@@ -1,65 +1,117 @@
+import asyncio
 import discord
 import random
-
-client = discord.Client()
-
-reply_simple = [
-    '哦',
-    '嗯',
-]
-
-reply_combinable = [
-    '收皮啦',
-    '食屎啦',
-    '關我咩事呢',
-    '關你咩事呢',
-    '瞓啦',
-]
-
-reply_suffix = [
-    '毒撚',
-    '毒L',
-    '柒頭',
-    '7頭',
-    '仆街',
-    'on9',
-]
-
-reply_emoji = [
-    '7.7',
-    '7.777',
-    '：）',
-    '：（',
-]
-
-reply_sticker = [
-    None
-]
+import time
 
 
-@client.event
-async def on_ready():
-    print('{0.user}駕到！'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$娘娘'):
-        final_message = ''
-        if random.randint(0,2):
-            final_message += random.choice(reply_combinable)
-            if random.randint(0,2):
-                final_message += random.choice(reply_suffix)
-        else:
-            final_message += random.choice(reply_simple)
-        if random.randint(0,1):
-            if random.randint(0,1):
-                final_message += random.choice(reply_emoji)
-            else:
-                final_message += random.choice(reply_sticker) or ''
+class Name(object):
+    content = ''
+    classifier_single = ''
+    classifier_multiple = ''
+    
+    def __init__(self, content, classifier_single='', classifier_multiple=''):
+        self.content = content
+        self.classifier_single = classifier_single
+        self.classifier_multiple = classifier_multiple
         
-        await message.channel.send(final_message)
+    def get(self, no_measure=False):
+        if not no_measure:
+            if self.classifier_multiple != '' and random.randint(0,1):
+                if random.randint(0,3):
+                    return self.classifier_multiple + self.content
+                return '你哋' + self.classifier_multiple + self.content
+            
+            elif self.classifier_single != '' and random.randint(0,1):
+                if random.randint(0,3):
+                    return self.classifier_single + self.content
+                return '你' + self.classifier_single + self.content
+            
+            return self.content
+        return self.content
 
-client.run('Put your token here la 7.7')
+
+class Sentence(object):
+    content = ''
+    prefix = False
+    suffix = False
+    particle = ''
+    no_measure = False
+    name = [
+        Name('毒撚', classifier_single='個', classifier_multiple='啲'),
+        Name('柒頭', classifier_single='條', classifier_multiple='啲'),
+        Name('on9', classifier_single='條', classifier_multiple='啲'),
+        Name('單身狗', classifier_single='隻', classifier_multiple='啲'),
+        Name('你'),
+    ]
+    emoji = ['7.7', '7.777', '：）', '：（']
+    
+    def __init__(self, content, prefix=False, suffix=False, particle='', no_measure=False):
+        self.content = content
+        self.prefix = prefix
+        self.suffix = suffix
+        self.particle = particle
+        self.no_measure = no_measure
+        
+    def get(self):
+        final_content = self.content
+        
+        if self.suffix and random.randint(0,1):
+            if self.particle != '':
+                final_content += self.particle
+            final_content += random.choice(self.name).get(no_measure=self.no_measure)
+        elif self.prefix:
+            final_content = random.choice(self.name).get(no_measure=self.no_measure) + final_content
+            
+        if random.randint(0,1):
+            final_content += ' ' + random.choice(self.emoji)
+        
+        return final_content
+
+
+class Fake_MKMui(discord.Client):
+    replies = [
+        Sentence('唔想理你'),
+        
+        Sentence('dont ff', prefix=True, no_measure=True),
+        Sentence('收皮', prefix=True, suffix=True, particle='啦', no_measure=True),
+        Sentence('食屎', prefix=True, suffix=True, particle='啦', no_measure=True),
+        Sentence('真係好撚恐怖', prefix=True),
+        
+        Sentence('關我咩事呢', suffix=True, no_measure=True),
+        Sentence('關你咩事呢', suffix=True, no_measure=True),
+        Sentence('瞓啦', suffix=True, no_measure=True),
+    ]
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    
+    async def on_connect(self):
+        print(f'{self.user}駕到！')
+        
+        
+    async def on_disconnect(self):
+        print(f'{self.user}斷線！')
+        raise RuntimeError('Disconnected.')
+        
+    
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith(('$娘娘', '$MK妹', '$mk妹')):
+            reply = random.choice(self.replies).get()
+            if random.randint(0,3):
+                await message.channel.send(reply)
+            else:
+                await message.reply(reply)
+
+
+while True:
+    try:
+        fake_mkmui = Fake_MKMui(loop=asyncio.new_event_loop())
+        fake_mkmui.run('Put your token here la 7.7')
+    except Exception as e:
+        print(f'Error: {e}\nRestarting in 10 seconds.')
+        time.sleep(10)
